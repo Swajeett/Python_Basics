12e44160efd920b79dba377f8422ec163c6655c0
Swajeett <swajitbhagwatsoftwareengineer@gmail.com>
2026-01-26 20:53:05 +0530

    Add files via upload

---
+++ Break_Continue.py
@@ -0,0 +1,13 @@
+# Break Statement
+
+for i in range (1, 10):
+    if i == 5:
+        break
+    print(i)
+
+# Continue Statement
+
+for i in range (1, 6):
+    if i == 3:
+        continue
+    print(i)
