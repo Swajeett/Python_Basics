74aef975cf7b32516128caeba9c778ca7b875400
Swajeett <swajitbhagwatsoftwareengineer@gmail.com>
2026-01-26 20:53:25 +0530

    Add files via upload

---
+++ For_Loop.py
@@ -0,0 +1,16 @@
+# Loop through range
+for i in range(5):
+    print(i)
+
+# Loop through list
+
+cars = ["BMW", "Audi", "Tesla"]
+
+for car in cars:
+    print(car)
+
+# With index
+
+for i in range (len(cars)):
+    print(i, cars[i])
+        
